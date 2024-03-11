318952774eca9c67dcd345311aedd0da51ac7e7c
Daniil Ugreninov <danik888844@gmail.com>
2024-03-11 14:17:49 +0500

    add: end of draft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import "./styles/main.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import ProjectPage from "./pages/ProjectPage";
+import Contacts from "./pages/Contacts";
+import ScrollToTop from "./utils/srollToTop";
 
 function App() {
   return (
     <div className="App">
-
-        <Navbar />
-
-        <Projects />
-
+      <Router>
+        <ScrollToTop />
+		    <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectPage />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
         <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/helpers/ProjectsList.js
@@ -0,0 +1,53 @@
+import projects01 from "./../img/projects/01.jpg";
+import projects01Big from "./../img/projects/01-big.jpg";
+import projects02 from "./../img/projects/02.jpg";
+import projects02Big from "./../img/projects/02-big.jpg";
+import projects03 from "./../img/projects/03.jpg";
+import projects03Big from "./../img/projects/03-big.jpg";
+import projects04 from "./../img/projects/04.jpg";
+import projects04Big from "./../img/projects/04-big.jpg";
+import projects05 from "./../img/projects/05.jpg";
+import projects05Big from "./../img/projects/05-big.jpg";
+import projects06 from "./../img/projects/06.jpg";
+import projects06Big from "./../img/projects/06-big.jpg";
+
+const projects = [
+    {
+        title: 'Gaming streaming portal',
+        skills: 'React Node Asd',
+        img: projects01,
+        imgBig: projects01Big
+    },
+    {
+        title: 'Project Number Two',
+        skills: 'React Node Asd',
+        img: projects02,
+        imgBig: projects02Big
+    },
+    {
+        title: 'Project Number Three',
+        skills: 'React Node Asd',
+        img: projects03,
+        imgBig: projects03Big
+    },
+    {
+        title: 'Project Number Four',
+        skills: 'React Node Asd',
+        img: projects04,
+        imgBig: projects04Big
+    },
+    {
+        title: 'Project Number Five',
+        skills: 'React Node Asd',
+        img: projects05,
+        imgBig: projects05Big
+    },
+    {
+        title: 'Project Number Six',
+        skills: 'React Node Asd',
+        img: projects06,
+        imgBig: projects06Big
+    },
+];
+
+export {projects}
